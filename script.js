a6d2ced46c30d28c36a73dfd94f3ca8a6b60c087
MANISH KUMARMISHRA <41567887+mm0895@users.noreply.github.com>
2018-10-21 10:18:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,8 @@ var color2 = document.querySelector(".color2");
 var css = document.querySelector("h3");
 var body = document.getElementById("gradient"); 
 function setGradient(){
-		body.style.background = "linear-gradient(to right, "
-
-			 + color1.value +
-		 ", " + color2.value + ")";
-		css.textContent = body.style.background + ";";
+		body.style.background ="linear-gradient(to right, " +color1.value+", " +color2.value+ ")";
+		css.textContent = body.style.background;
 		  }
 
 color1.addEventListener("input", setGradient);
